c74dba75ea2a90ed6adc79fe4f035dd6e6b81fdc
KirillUshakov <cool.ushackov2016@yandex.ru>
2022-12-02 16:18:27 +0300

    Setup styles / assets / routes

--- src/router/index.js
+++ src/router/index.js
@@ -8,13 +8,23 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/todo-list/",
+    name: "todo-list",
+    children: [
+      {
+        path: "",
+        component: () => import("../views/TodoListView.vue"),
+      },
+      {
+        path: ":id",
+        component: () => import("../views/TodoListView.vue"),
+      },
+    ],
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "e404",
+    component: () => import("../views/NotFoundView.vue"),
   },
 ];
 
