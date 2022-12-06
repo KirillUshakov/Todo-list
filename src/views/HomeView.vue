e1423725fe3901cb8858040bff74f17ec5b56559
KirillUshakov <cool.ushackov2016@yandex.ru>
2022-12-06 21:25:07 +0300

    Complete homeView and Card module

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,11 @@
     <div class="container">
       <div class="header">
         <h1 class="title">Lists</h1>
-        <button class="btn round-btn round-btn--alt-theme" aria-label="Add list">
+        <button
+          @click="$router.push('/todo-list/')"
+          class="btn round-btn round-btn--alt-theme"
+          aria-label="Add list"
+        >
           <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M2 12.8399H23.6667" stroke="#4F4F4F" stroke-width="3" stroke-linecap="round"/>
             <path d="M12.8398 23.6667L12.8398 2.00001" stroke="#4F4F4F" stroke-width="3" stroke-linecap="round"/>
@@ -13,9 +17,9 @@
       <div class="content">
         <div class="card-grid">
           <todo-list-card
-            v-for="(card, index) in 10"
-            :key="index"
-            :id="index"
+            v-for="todolist in todoListArr"
+            :key="todolist.id"
+            :list="todolist"
           />
         </div>
       </div>
@@ -30,7 +34,15 @@ export default {
 </script>
 
 <script setup>
+import { useStore } from 'vuex'
+import { computed } from '@vue/runtime-core';
 import TodoListCard from "@/components/Card";
+
+const store = useStore();
+const getters = store.getters;
+
+const todoListArr = computed(() => getters.todoListArr);
+
 </script>
 
 <style lang="scss" scoped>
